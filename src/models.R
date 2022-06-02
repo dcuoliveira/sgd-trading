554181ff81448623cccd61a9a6fe876b7d96ba2c
dcuoliveira <dan.cunhaa@gmail.com>
2022-06-01 21:47:15 -0300

    add rolling reg and draft of strategy

--- src/models.R
+++ src/models.R
@@ -1,24 +1,39 @@
+library("rollRegres")
+library('here')
+library('dplyr')
+library("tidyr")
+library("data.table")
+library("roll")
+library("rlang")
+
+
 roll_reg_prop = function(formula,
+                         target_name,
                          data,
-                         window_size,
+                         reg_window_size,
+                         mean_window_size,
                          do_compute=c("sigmas", "r.squareds", "1_step_forecasts")){
-  date = row.names(data)
   
+  date = data %>% select(date)
+  data = data %>% select(-date)
   rollingreg = roll_regres(formula = formula,
                            data = data,
-                           width = window_size,
+                           width = reg_window_size,
                            do_compute = do_compute)
-  rollingreg$dates = date
-  rollingreg$coefs = rollingreg$coefs %>% as.data.table() %>% mutate(date=date) %>% select(date, everything())
+  rollingreg$date = date
+  tmp_coefs = rollingreg$coefs %>% as.data.table() %>%
+    mutate(date=ymd(row.names(rollingreg$coefs))) %>%
+    select(date, everything())
+  rollingreg$coefs = tmp_coefs
   
   fits = list()
   resids = list()
   for (i in 1:dim(rollingreg$coefs %>% drop_na())[1]){
     dtref = rollingreg$coefs %>% drop_na() %>% slice(i) %>% select(date)
     tmp_coef = rollingreg$coefs %>% drop_na() %>% slice(i) %>% select(-date) %>% as.numeric()
-    tmp_X = data %>% filter(row.names(data) == dtref[[1]]) %>% select(-contains(TARGET)) %>% as.numeric()
+    tmp_X = data %>% filter(row.names(data) == dtref[[1]]) %>% select(-contains(target_name)) %>% as.numeric()
     tmp_fit = t(tmp_coef) %*% tmp_X
-    tmp_resid = data %>% filter(row.names(data) == dtref[[1]]) %>% select(contains(TARGET)) - tmp_fit
+    tmp_resid = data %>% filter(row.names(data) == dtref[[1]]) %>% select(contains(target_name)) - tmp_fit
     
     tmp_resid$date = rownames(tmp_resid)
     tmp_resid = tmp_resid %>% select(date, everything())
@@ -30,12 +45,19 @@ roll_reg_prop = function(formula,
     fits[[i]] = tmp_fit %>% as.data.table()
     resids[[i]] = tmp_resid %>% as.data.table()
   }
+
+  fits_df = do.call("rbind", fits) %>%  as.data.table() %>% rename(!!paste0(TARGET, "_fit") := "fit") %>%
+    mutate(date=ymd(date))
   
-  fits_df = do.call("rbind", fits) %>%  as.data.table()
-  resids_df = do.call("rbind", resids) %>%  as.data.table()
-  
-  resids_df[,paste0(TARGET, "_zscore")] = (resids_df$SGD - roll_mean(resids_df$SGD, width = MEAN_WINDOW_SIZE)) / roll_sd(resids_df$SGD, width = MEAN_WINDOW_SIZE)
-  
+  resids_df = do.call("rbind", resids) %>%  as.data.table() %>%
+    mutate(date=ymd(date),
+           !!paste0(target_name, "_roll_mean") := roll_mean(!!sym(target_name), width = mean_window_size),
+           !!paste0(target_name, "_roll_sd") := roll_sd(!!sym(target_name), width = mean_window_size)) %>%
+    mutate(!!paste0(target_name, "_zscore") := (!!sym(paste0(target_name)) - !!sym(paste0(target_name, "_roll_mean"))) / !!sym(paste0(target_name, "_roll_sd"))) %>%
+    rename(!!paste0(TARGET, "_resid") := !!paste0(TARGET)) %>% 
+    select(date, !!paste0(target_name, "_resid"), !!paste0(target_name, "_zscore")) %>%
+    mutate(!!paste0(target_name, "_zscore") := coalesce(!!sym(paste0(target_name, "_zscore")), 0))
+ 
   rollingreg$fits = fits_df
   rollingreg$resids = resids_df
   
--- src/rolling_regression_strategy.R
+++ src/rolling_regression_strategy.R
@@ -5,34 +5,70 @@ library('dplyr')
 library("tidyr")
 library("data.table")
 library("roll")
+library("rlang")
 
 source(here('src', 'utils.R'))
 source(here('src', 'models.R'))
 source(here('src', 'plot_funcs.R'))
 
-MODEL = "rolling_reg"
-OUTPUT_PATH = here("src", "data", "outputs", "models", MODEL)
-WINDOW_SIZE = 52 * 4
-MEAN_WINDOW_SIZE = 52 * 1
-INTERCEPT = FALSE
-TARGET = "SGD"
+MODEL <- "rolling_reg"
+OUTPUT_PATH <- here("src", "data", "outputs", "models", MODEL)
+WINDOW_SIZE <- 52 * 4
+MEAN_WINDOW_SIZE <- 52 * 1
+INTERCEPT <- FALSE
+TARGET <- "SGD"
 
-data = merge_fx_sneer_data()
-data_orig = data %>% select(-date)
-date = data$date
-data =  data %>% select(-date)
-# data =  data %>% select(-date) %>% apply(2, function(x) scale(x)) %>% as.data.frame()
+data <- merge_fx_sneer_data()
+# data <-  data %>% select(-date) %>% apply(2, function(x) scale(x)) %>% as.data.frame()
 
 if (INTERCEPT == T){
-  model_formula = paste("SGD", "~", paste(names(data)[-grep("SGD|SNEER", names(data))], collapse=" + "))
+  model_formula <- paste("SGD", "~", paste(names(data)[-grep("SGD|SNEER|date", names(data))], collapse=" + "))
 }else{
-  model_formula = paste(paste("SGD", "~", paste(names(data)[-grep("SGD|SNEER", names(data))], collapse=" + ")), " -1")
+  model_formula <- paste(paste("SGD", "~", paste(names(data)[-grep("SGD|SNEER|date", names(data))], collapse=" + ")), " -1")
 }
 
-rollingreg = roll_reg_prop(formula = model_formula,
-                           data = data %>% select(-SNEER),
-                           window_size = WINDOW_SIZE,
-                           do_compute=c("sigmas", "r.squareds", "1_step_forecasts"))
+rollingreg <- roll_reg_prop(formula = model_formula,
+                            target_name=TARGET,
+                            data = data %>% select(-SNEER),
+                            reg_window_size = WINDOW_SIZE,
+                            mean_window_size = MEAN_WINDOW_SIZE,
+                            do_compute=c("sigmas", "r.squareds", "1_step_forecasts"))
 
+threshold <- 1.5
+positions_df <- rollingreg$resids
+coefs_df <- rollingreg$coefs
+coef_names <- colnames(coefs_df %>% select(-date))
 
+# target positions
+positions_df <- positions_df %>% mutate(target_position := ifelse(!!sym(paste0(TARGET, "_zscore")) > threshold, 1,
+                                                                 ifelse(!!sym(paste0(TARGET, "_zscore")) < -threshold,
+                                                                        -1,
+                                                                        0)))
 
+# covariates positions
+merge_coefs_df <- merge(positions_df, coefs_df, by = "date")
+expanded_target_positions <- replicate(length(coef_names), (merge_coefs_df %>% select(target_position)) * -1)
+coefs_matrix <- merge_coefs_df %>% select(all_of(coef_names))
+new_coefs_df <- -1 * coefs_matrix
+new_coefs_df$date <- merge_coefs_df$date
+new_coefs_df <- new_coefs_df %>% select(date, everything())
+
+# prepare lead returns
+positions_df <- merge(positions_df, new_coefs_df, by = "date")
+ret_df <- data %>% select(-date, -SNEER) %>% lapply(function(x) (x/shift(x, 1)-1)) %>% as.data.table() %>% lead() %>%
+  mutate(date=data$date) %>% select(date, everything())
+
+# merge lead returns and positions
+target_positions_df <- positions_df %>% rename(!!TARGET := target_position) %>% select(date, append(TARGET, coef_names))
+target_ret_df <- ret_df %>% select(date, append(TARGET, coef_names)) %>% filter(date %in% target_positions_df$date)
+
+output_df <- (target_positions_df %>% select(-date)) * (target_ret_df %>% select(-date))
+output_df$date <- target_ret_df$date
+output_df <- output_df %>% select(date, everything())
+output_df$portfolio <- sum(output_df %>% select(-date))
+
+cumprod_output_df <- cumprod(1+(output_df %>% select(-date))) %>% as.data.table() %>% mutate(date=output_df$date) %>%
+  select(date, everything())
+
+melt_cumprod_output_df <- melt(cumprod_output_df, id="date")
+ggplot(melt_cumprod_output_df, aes(x=date, y=value, colour=variable, group=variable)) + geom_line()
