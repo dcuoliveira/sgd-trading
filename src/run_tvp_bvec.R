476d75e7f7928df55365787b1a775e73423ce4c1
dcuoliveira <dan.cunhaa@gmail.com>
2024-05-26 11:33:44 -0300

    add argparser

--- src/run_tvp_bvec.R
+++ src/run_tvp_bvec.R
@@ -9,23 +9,56 @@ library("dlm")
 library("bvartools")
 library("here")
 library("parallel")
+library("optparse")
 
 source(file.path(here(), 'src', 'models', 'utils.R'))
 source(file.path(here(), 'src', 'models', 'models.R'))
 source(file.path(here(), 'src', 'plots', 'plot_funcs.R'))
 
-MODEL <- "tvp-bvec"
-OUTPUT_PATH <- file.path(here(), 'src', 'data', 'outputs', MODEL)
-WINDOW_SIZE <- 52 * 2
-MEAN_WINDOW_SIZE <- 52 * 1
-INTERCEPT <- TRUE
-TARGET <- "SGD"
-SCALE_TYPE <- "rolling_scale"
-num_cores <- detectCores() - 1
+# define command-line options
+option_list <- list(
+  make_option(c("--model_name"), type = "character", help = "Model name for output", default = "tvp-bvec"),
+  make_option(c("--output_path"), type = "character", help = "Output path", default = file.path(here(), 'src', 'data', 'outputs', "tvp-bvec")),
+  make_option(c("--window_size"), type = "integer", help = "Window size", default = 52 * 2),
+  make_option(c("--mean_window_size"), type = "integer", help = "Mean window size", default = 52 * 1),
+  make_option(c("--intercept"), type = "logical", help = "Intercept", default = TRUE),
+  make_option(c("--scale_type"), type = "character", help = "Scale type", default = "rolling_scale"),
+  make_option(c("--num_cores"), type = "integer", help = "Number of cores", default = detectCores() - 1)
+)
 
+# create a parser object
+parser <- OptionParser(option_list = option_list)
+
+# parse the arguments
+args <- parse_args(parser)
+
+MODEL <- args$model_name
+OUTPUT_PATH <- args$output_path
+WINDOW_SIZE <- args$window_size
+MEAN_WINDOW_SIZE <- args$mean_window_size
+SCALE_TYPE <- args$scale_type
+num_cores <- args$num_cores
+
+# load data
 data <- load_and_resample_currencies() %>% mutate(date=ymd(date)) %>% filter(date >= "2006-01-01")
 data_orig <- data
 data <- data %>% select(-date) # %>% apply(2, function(x) scale(x)) %>% as.data.frame()
+
+# scale data
+if (SCALE_TYPE == "scale"){
+  data <- data %>% lapply(scale) %>% as.data.table()
+  row.names(data) <- data_orig$date
+}else if (SCALE_TYPE == "rolling_scale"){
+  n <- dim(data)[1]
+  mean_data <- apply(data, 2, function(x) {roll_mean(x, width = n, min_obs = WINDOW_SIZE)})
+  sd_data <- apply(data, 2, function(x) {roll_sd(x, width = n, min_obs = WINDOW_SIZE)})
+  
+  data <- ((data - mean_data) / sd_data)
+  row.names(data) <- data_orig$date
+  data <- data %>% drop_na()
+}
+
+# change to ts format
 tsdata <- ts(data, start = 1, end = nrow(data), frequency = 52)
 
 # generate BVEC model
