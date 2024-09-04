128c8f111d4e38565b34436029d133b374e515eb
dcuoliveira <dan.cunhaa@gmail.com>
2024-09-04 08:12:21 -0300

    add old backtest

--- src/run_cuzzi_backtest.R
+++ src/run_cuzzi_backtest.R
@@ -1,124 +1,144 @@
-
 rm(list=ls())
-library("here")
-library("dplyr")
-library("roll")
+library('here')
+library('dplyr')
+library("tidyr")
+library("data.table")
+library("MTS")
+library("lubridate")
+library("optparse")
 
-source(file.path(here(), 'src', 'models', 'utils.R'))
+source(file.path(getwd(), 'src', 'models', 'utils.R'))
+source(file.path(getwd(), 'src', 'models', 'models.R'))
+source(file.path(getwd(), 'src', 'plots', 'plot_funcs.R'))
 
-window_size <- 2 * 52 
-freq <- "weekly"
+# define command-line options
+option_list <- list(
+  make_option(c("--model_name"), type = "character", help = "Model name for output", default = "dlm"),
+  make_option(c("--output_path"), type = "character", help = "Output path", default = file.path(here(), 'src', 'data', 'outputs')),
+  make_option(c("--frequency"), type = "character", help = "Frequency to parse the data", default = "weekly"),
+  make_option(c("--intercept"), type = "logical", help = "Intercept", default = FALSE),
+  make_option(c("--scale_type"), type = "character", help = "Scale type", default = "rolling_scale"),
+  make_option(c("--strategy_type"), type = "character", help = "Strategy type", default = "scale"),
+  make_option(c("--window_size"), type = "integer", help = "Window size", default = 52 * 4),
+  make_option(c("--target"), type = "character", help = "Target variable name", default = "SGD"),
+  make_option(c("--betas_type"), type = "character", help = "Betas type", default = NULL),
+  make_option(c("--threshold"), help = "Threshold to the used on the strategy", default = 2)
+)
 
-K <- 1
-initial_cash <- 100
-trade_size <- 10
+# create a parser object
+parser <- OptionParser(option_list = option_list)
 
-rolling_ols <- function(y, x, window_size, intercept = TRUE) {
-    n <- dim(y)[1]
-    betas <- matrix(NA, nrow = n, ncol = ifelse(intercept, ncol(x) + 1, ncol(x)))
-    residuals <- numeric(n)
+# parse the arguments
+args <- parse_args(parser)
 
-    for (i in window_size:n) {
-        window_x <- x[(i - window_size + 1):i, , drop = FALSE] %>% as.matrix()
-        window_y <- y[(i - window_size + 1):i, ]
+MODEL <- args$model_name
+STRATEGY_TYPE <- args$strategy_type
+OUTPUT_PATH <- file.path(args$output_path, MODEL)
+TARGET <- args$target
+SCALE_TYPE <- args$scale_type
+BETAS_TYPE <- "filter"
+FREQ = args$frequency
+WINDOW_SIZE <- args$window_size
+THRESHOLD <- as.numeric(args$threshold)
+INTERCEPT <- args$intercept
+STRATEGY_TYPE <- args$strategy_type
 
-        if (intercept) {
-            model <- lm(window_y ~ window_x)
-        } else {
-            model <- lm(window_y ~ window_x + 0)
-        }
-
-        betas[i, ] <- coef(model) %>% as.matrix() %>% t()
-        residuals[i] <- model$residuals[length(model$residuals)]
-    }
-
-    return(list(betas = betas, residuals = residuals))
+if (FREQ == "monthly"){
+  FREQ_INT <- 12
+}else if (FREQ == "weekly"){
+  FREQ_INT <- 52
 }
 
-data <- load_and_resample_currencies(freq=freq) %>% mutate(date=ymd(date)) %>% filter(date >= "2006-01-01")
+if (INTERCEPT == T){
+  intercept_tag <- "intercept"
+}else{
+  intercept_tag <- "nointercept"
+}
 
-x_data <- data %>% select(KRW, MYR, CNY, THB, IDR, TWD, INR, JPY, AUD, GBP, EUR)
-y_data <- data %>% select(SGD)
+# prices data
+prices_df <- load_and_resample_currencies(freq=FREQ) %>% mutate(date=ymd(date)) # %>% filter(date >= "2006-01-01")
 
-rolling_results_with_intercept <- rolling_ols(y_data, x_data, window_size, intercept = TRUE)
-
-rolling_results_no_intercept <- rolling_ols(y_data, x_data, window_size, intercept = FALSE)
-
-# ect_with_intercept <- scale(rolling_results_with_intercept$residuals)
-# ect_no_intercept <- scale(rolling_results_no_intercept$residuals)
-
-ect_with_intercept <- rolling_results_with_intercept$residuals %>% as.data.table()
-ect_no_intercept <- rolling_results_no_intercept$residuals %>% as.data.table()
-
-n = dim(ect_with_intercept)[1]
-mean_data_with_intercept <- apply(ect_with_intercept, 2, function(x) {roll_mean(x, width = window_size, min_obs = window_size)})
-sd_data_with_intercept <- apply(ect_with_intercept, 2, function(x) {roll_sd(x, width = window_size, min_obs = window_size)})
-ect_with_intercept <- ((ect_with_intercept - mean_data_with_intercept) / sd_data_with_intercept)
-ect_with_intercept <- ect_with_intercept %>% drop_na() %>% as.matrix()
-
-n = dim(ect_no_intercept)[1]
-mean_data_with_no_intercept <- apply(ect_no_intercept, 2, function(x) {roll_mean(x, width = window_size, min_obs = window_size)})
-sd_data_with_no_intercept <- apply(ect_no_intercept, 2, function(x) {roll_sd(x, width = window_size, min_obs = window_size)})
-ect_no_intercept <- ((ect_no_intercept - mean_data_with_no_intercept) / sd_data_with_no_intercept)
-ect_no_intercept <- ect_no_intercept %>% drop_na() %>% as.matrix()
-
-# TRADING STRATEGY
+if (MODEL == "dlm"){
+  
+  # load model output
+  model_out <- readRDS(file = file.path(OUTPUT_PATH, SCALE_TYPE, paste0("model_results_", FREQ, "_", WINDOW_SIZE, "_", intercept_tag, ".rds")))
+  
+  residuals_df = model_out$residuals$res %>% as.data.table()
+  n = dim(residuals_df)[1]
+  # mean <- rollapply(residuals_df$residual, width = WINDOW_SIZE, FUN = mean, align = "right", partial = TRUE)
+  std <- rollapply(residuals_df$residual, width = WINDOW_SIZE, FUN = sd, align = "right", partial = TRUE)
+  # residuals <- ((residuals_df$residual - mean) / sd)
+  # residuals <- residuals %>% as.matrix()
+  # cointegration_error_df = data.table(date=residuals_df$date, residual=residuals_df$residual) %>%
+  #   rename(residual=`residual.V1`) %>%
+  #   mutate(ub=THRESHOLD, lb=-THRESHOLD)
+  cointegration_error_df = residuals_df %>% mutate(std=std) %>% mutate(ub=THRESHOLD*std, lb=-THRESHOLD*std)
+  
+  # betas
+  if (BETAS_TYPE == "smooth"){
+    betas_df <- model_out$smooth$s
+  }else{
+    betas_df <- model_out$filter$m
+  }
+  
+}else if (MODEL == "rolling-ols"){
+  
+  # load model output
+  model_out <- readRDS(file = file.path(OUTPUT_PATH, SCALE_TYPE, paste0("model_results_", FREQ, "_", WINDOW_SIZE, "_", intercept_tag, ".rds")))
+  
+  # cointegration error
+  residuals = model_out$residuals$residual %>% as.data.table()
+  n = dim(residuals)[1]
+  mean <- apply(residuals, 2, function(x) {roll_mean(x, width = WINDOW_SIZE, min_obs = WINDOW_SIZE)})
+  sd <- apply(residuals, 2, function(x) {roll_sd(x, width = WINDOW_SIZE, min_obs = WINDOW_SIZE)})
+  residuals <- ((residuals - mean) / sd)
+  residuals <- residuals %>% as.matrix()
+  cointegration_error_df = data.table(date=model_out$residuals$date, residual=residuals) %>%
+    rename(residual=`residual..`) %>%
+    mutate(ub=THRESHOLD, lb=-THRESHOLD)
+  
+  # betas
+  betas_df <- model_out$model$coefs %>% as.data.table() %>%
+    mutate(date=cointegration_error_df$date) %>% select(date, everything()) %>% as.data.table()
+  
+}
 
 trade_strategy <- function(ect, initial_cash, trade_size, K) {
-    position <- 0
-    cash <- initial_cash
-    portfolio_value <- numeric(length(ect))
-    positions <- numeric(length(ect))
-    portfolio_value[1] <- initial_cash
-
-    for (i in 2:length(ect)) {
-        if (ect[i-1] <= -K && position == 0) {
-            position <- trade_size
-            cash <- cash - (position * ect[i-1])
-        } else if (ect[i-1] >= K && position == 0) {
-            position <- -trade_size
-            cash <- cash - (position * ect[i-1] )
-        } else if (ect[i-1] * sign(position) > 0 && abs(ect[i-1] ) < abs(K)) {
-            cash <- cash + (position * ect[i-1])
-            position <- 0
-        }
-        portfolio_value[i] <- cash + (position * ect[i-1])
-        positions[i] <- position
+  position <- 0
+  cash <- initial_cash
+  portfolio_value <- numeric(length(ect))
+  positions <- numeric(length(ect))
+  portfolio_value[1] <- initial_cash
+  
+  for (i in 2:length(ect)) {
+    if (ect[i-1] <= -K && position == 0) {
+      position <- trade_size
+      cash <- cash - (position * ect[i-1])
+    } else if (ect[i-1] >= K && position == 0) {
+      position <- -trade_size
+      cash <- cash - (position * ect[i-1] )
+    } else if (ect[i-1] * sign(position) > 0 && abs(ect[i-1] ) < abs(K)) {
+      cash <- cash + (position * ect[i-1])
+      position <- 0
     }
-    
-    list(portfolio_value = portfolio_value, cash = cash, position = position)
+    portfolio_value[i] <- cash + (position * ect[i-1])
+    positions[i] <- position
+  }
+  
+  list(portfolio_value = portfolio_value, cash = cash, position = position)
 }
 
-strategy_no_intercept <- trade_strategy(ect_no_intercept, initial_cash, trade_size, K)
-strategy_with_intercept <- trade_strategy(ect_with_intercept, initial_cash, trade_size, K)
+strategy <- trade_strategy(ect = cointegration_error_df$residual,
+                           initial_cash = 100,
+                           trade_size = 10,
+                           K = 1)
 
-strategy_returns_no_intercept <- (strategy_no_intercept$portfolio_value - lag(strategy_no_intercept$portfolio_value)) / lag(strategy_no_intercept$portfolio_value) # diff(strategy_no_intercept$portfolio_value) / strategy_no_intercept$portfolio_value[-length(strategy_no_intercept$portfolio_value)]
-annualized_return_no_intercept <- mean(strategy_returns_no_intercept, na.rm = TRUE) * 52
-annualized_volatility_no_intercept <- sd(strategy_returns_no_intercept, na.rm = TRUE) * sqrt(52)
-sharpe_ratio_no_intercept <- annualized_return_no_intercept / annualized_volatility_no_intercept
+strategy_returns <- (strategy$portfolio_value - lag(strategy$portfolio_value)) / lag(strategy$portfolio_value)
+annualized_return <- mean(strategy_returns, na.rm = TRUE) * 52
+annualized_volatility <- sd(strategy_returns, na.rm = TRUE) * sqrt(52)
+sharpe_ratio <- annualized_return / annualized_volatility
+print(sharpe_ratio)
 
-strategy_returns_with_intercept <- (strategy_with_intercept$portfolio_value - lag(strategy_with_intercept$portfolio_value)) / lag(strategy_with_intercept$portfolio_value) # diff(strategy_with_intercept$portfolio_value) / strategy_with_intercept$portfolio_value[-length(strategy_with_intercept$portfolio_value)]
-annualized_return_with_intercept <- mean(strategy_returns_with_intercept, na.rm = TRUE) * 52
-annualized_volatility_with_intercept <- sd(strategy_returns_with_intercept, na.rm = TRUE) * sqrt(52)
-sharpe_ratio_with_intercept <- annualized_return_with_intercept / annualized_volatility_with_intercept
 
-print(paste0("SharpeNoIntercept=", sharpe_ratio_no_intercept, "      SharpeIntercept=", sharpe_ratio_with_intercept))
 
-# df_no_intercept_value <- data.frame(Time = Date, PortfolioValue = strategy_no_intercept$portfolio_value, Model = "No Intercept")
-# df_with_intercept_value <- data.frame(Time = Date, PortfolioValue = strategy_with_intercept$portfolio_value, Model = "With Intercept")
-# df_combined_value <- bind_rows(df_no_intercept_value, df_with_intercept_value)
 
-# ggplot(df_combined_value, aes(x = Time, y = PortfolioValue, color = Model)) +
-#   geom_line(size = 1) +
-#   labs(title = "Mean Reversion Strategy Performance",
-#        x = "Time",
-#        y = "Portfolio Value (USD)") +
-#   theme_minimal() +
-#   theme(text = element_text(size = 12)) +
-#   scale_color_manual(values = c("blue", "red")) +
-#   annotate("text", x = max(df_combined_value$Time) - 10, y = min(df_combined_value$PortfolioValue), 
-#            label = sprintf("Sharpe Ratio (No Intercept): %.2f", sharpe_ratio_no_intercept), 
-#            color = "blue", hjust = 1) +
-#   annotate("text", x = max(df_combined_value$Time) - 10, y = min(df_combined_value$PortfolioValue) + 50, 
-#            label = sprintf("Sharpe Ratio (With Intercept): %.2f", sharpe_ratio_with_intercept), 
-#            color = "red", hjust = 1)
