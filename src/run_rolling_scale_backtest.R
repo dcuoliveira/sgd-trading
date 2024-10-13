d0116d052c317daf606340a052eccc3872c31846
dcuoliveira <dan.cunhaa@gmail.com>
2024-10-13 08:57:49 +0100

    change defaulta parameteres

--- src/run_rolling_scale_backtest.R
+++ src/run_rolling_scale_backtest.R
@@ -19,13 +19,13 @@ option_list <- list(
   make_option(c("--output_path"), type = "character", help = "Output path", default = file.path(here(), 'src', 'data', 'outputs')),
   make_option(c("--frequency"), type = "character", help = "Frequency to parse the data", default = "weekly"),
   make_option(c("--intercept"), type = "logical", help = "Intercept", default = FALSE),
-  make_option(c("--scale_type"), type = "character", help = "Scale type", default = "noscale"),
-  make_option(c("--strategy_type"), type = "character", help = "Strategy type", default = "rolling_scale"),
-  make_option(c("--window_size"), type = "integer", help = "Window size", default = 52 * 10),
-  make_option(c("--ma_window_size"), type = "integer", help = "Moving Average Window Size", default = 52 * 2),
+  make_option(c("--scale_type"), type = "character", help = "Scale type", default = "rolling_scale"),
+  make_option(c("--strategy_type"), type = "character", help = "Strategy type", default = "sd"),
+  make_option(c("--window_size"), type = "integer", help = "Window size", default = 52 * 2),
+  make_option(c("--ma_window_size"), type = "integer", help = "Moving Average Window Size", default = 52 * 1),
   make_option(c("--target"), type = "character", help = "Target variable name", default = "SGD"),
   make_option(c("--betas_type"), type = "character", help = "Betas type", default = NULL),
-  make_option(c("--threshold"), help = "Threshold to the used on the strategy", default = 0.5)
+  make_option(c("--threshold"), help = "Threshold to the used on the strategy", default = 1.5)
 )
 
 # create a parser object
@@ -85,11 +85,6 @@ if (MODEL == "dlm"){
   cointegration_error_df <- residuals_df %>% filter(date >= start_date & date <= end_date) %>% as.data.table()
   betas_df <- betas_df %>% filter(date >= start_date & date <= end_date) %>% as.data.table()
   
-  n = dim(residuals_df)[1]
-  mean <- rollapply(residuals_df$residual, width = MA_WINDOW_SIZE, FUN = mean, align = "right", partial = TRUE)
-  std <- rollapply(residuals_df$residual, width = MA_WINDOW_SIZE, FUN = sd, align = "right", partial = TRUE)
-  
-  cointegration_error_df$residual <- (cointegration_error_df$residual - mean) / std
   new_std <- rollapply(cointegration_error_df$residual, width = MA_WINDOW_SIZE, FUN = sd, align = "right", partial = TRUE)
   cointegration_error_df = cointegration_error_df %>%
     mutate(ub=THRESHOLD*new_std, lb=-THRESHOLD*new_std) %>%
@@ -120,7 +115,14 @@ prices_df <- prices_df %>% filter(date >= start_date & date <= end_date) %>% as.
 betas_df <- betas_df %>% filter(date >= start_date & date <= end_date) %>% as.data.table()
 
 # prepare signal
-signal <- build_signal_from_bounds(cointegration_error_df)
+# signal <- build_signal_from_bounds(cointegration_error_df)
+signal <- data.table(
+  date = cointegration_error_df$date
+)
+signal[, "signal" := ifelse(
+  cointegration_error_df$residual >= cointegration_error_df$ub, -1,
+  ifelse(cointegration_error_df$residual < cointegration_error_df$lb, 1, 0)
+)]
 
 # run backtest
 output <- run_backtest(signal=signal, betas=betas_df, prices=prices_df, target_name="SGD")
@@ -133,6 +135,7 @@ cum_vol_adj_returns_df <- output$cum_vol_adj_portfolio_returns
 positions_df <- output$positions
 
 outputs <- list(
+  cointegration_error=cointegration_error_df,
   positions=positions_df,
   pnl=pnl_df,
   cum_pnl=cum_pnl_df,
